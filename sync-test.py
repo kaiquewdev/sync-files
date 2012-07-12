a53822158bb8e5505475c24b0852cec340287b43
Kaique da Silva <kaique.developer@gmail.com>
2012-07-12 17:52:39 -0300

    copy to destiny in progress

--- sync-test.py
+++ sync-test.py
@@ -2,19 +2,28 @@ import unittest, sync
 
 class SyncTest( unittest.TestCase ):
     def default_path( self ):
-        return '/Users/kaiquesilva/Dropbox/PhoenixProjects/PDFs/gordo'
+        return '/Users/kaiquesilva/Dropbox/PhoenixProjects/PDFs'
 
     def test_open_file_format_json( self ):
-        file_path = '%s/update.json' % ( self.default_path() )
+        file_path = '%s/gordo/update.json' % ( self.default_path() )
         file_test = sync.get_json( file_path ) 
 
         self.assertEquals( True, len( file_test ) > 0 )
 
-    def test_sync_files( self ):
-        file_path = '%s/update.json' % ( self.default_path() )
+    def test_copy_of_a_file( self ):
+        import os
+        origin_path = './test_files/go-board.txt'
+        destiny_path = './test_files/go-board-rules.txt'
+
+        self.assertEquals( True, sync.copy( origin_path, destiny_path ) )
+
+        os.remove( destiny_path )
+
+    def sync_files( self ):
+        file_path = '%s/gordo/update.json' % ( self.default_path() )
         update_format = sync.get_json( file_path )
 
-        self.assertEquals( True, sync.files( update_format, self.default_path() ) )
+        self.assertEquals( True, sync.files( update_format, self.default_path, 'gordo' ) )
         
 if __name__ == '__main__':
     unittest.main()
--- sync.py
+++ sync.py
@@ -25,18 +25,58 @@ def get_json( filename='' ):
     except Exception:
         return output
 
-def files( sync_format='', action_directory='' ):
+def copy( origin='', destiny='' ):
+    output = False
+
+    try:
+        import os, shutil
+
+        if os.path.exists( origin ) and not os.path.exists( destiny ):
+            shutil.copy( origin, destiny )
+
+            if os.path.exists( destiny ):
+                output = True
+
+        return output
+    except Exception:
+        return output
+
+def files( sync_format='', main_directory='', action_directory='' ):
+    import pdb; pdb.set_trace()
     output = False
 
     try:
         import os
-
-        if sync_format and os.path.exists( action_directory ):
+        
+        if sync_format and os.path.exists( main_directory ) and os.path.exists( '%s/%s' % ( main_directory, action_directory ) ):
+            # Go to directory working directory
+            os.chdir( main_directory ) 
             
+            if action_directory in os.listdir('./'):
+                for reference in sync_format:
+                    paths = reference['link'].split('/')[3:]
+                    destiny_path = ''
+    
+                    for path in paths:
+                        destiny_path += path
 
+                        if path in os.listdir('./'):
+                            os.chdir( path )
+                        if not path in os.listdir('./') and not paths[-1:] == path:
+                            os.mkdir( './%s' % ( path ) )
+                        if paths[-1:] == path and not os.exists( './%s' % ( path ) ):
+                            output = copy( 
+                                ( '%s/%s/%s' % ( main_directory, action_directory, path ) ),
+                                ( destiny_path )
+                            )
+
+                            
+                output = sync_format
+        return output
     except Exception:
         return output
 
 if __name__ == '__main__':
-    pass
+    default_path = '/Users/kaiquesilva/Dropbox/PhoenixProjects/PDFs'
+    print files( get_json( '%s/gordo/update.json' % ( default_path ) ), default_path, 'gordo')
     # print get_json('/Users/kaiquesilva/Dropbox/PhoenixProjects/PDFs/gordo/update.json')
